7cb3a556464dce5d9e2e01abac235e540be6ed05
dhananjay1405 <dhananjay1405@outlook.com>
2024-06-28 14:23:04 +0530

    version: 1.0.28

--- platform/powershell/sync-multiple-company.ps1
+++ platform/powershell/sync-multiple-company.ps1
@@ -1,6 +1,6 @@
 $src = Import-Csv -Path .\config.csv
 foreach ($item in $src)
 {
-    Start-Process -FilePath node.exe -WorkingDirectory .\ -ArgumentList ".\dist\index.js --database-schema $($item.schema) --tally-company ""$($item.company)""" -Wait
+    Start-Process -FilePath node.exe -WorkingDirectory .\ -ArgumentList ".\dist\index.mjs --database-schema $($item.schema) --tally-company ""$($item.company)""" -Wait
     
 }
